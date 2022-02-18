33860f2ee1c0d13caaeefbb2308cd0b135c18c5c
tomsonas3000 <tomasjorudas23@gmail.com>
2022-02-18 18:58:08 +0200

    update user type entity configuration

---
+++ backend/ReservationSystem.DataAccess/TypeConfigurations/IdentityRoleTypeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReservationSystem.DataAccess.Enums;
+
+namespace ReservationSystem.DataAccess.TypeConfigurations
+{
+    public class IdentityRoleTypeConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(new IdentityRole
+            {
+                Id = "8D411ABA-7E5E-4FF9-9AFF-430B89BEB461",
+                Name = UserRole.Admin.ToString(),
+                ConcurrencyStamp = "1",
+                NormalizedName = UserRole.Admin.ToString().ToUpper()
+            }, new IdentityRole
+            {
+                Id = "32928326-7E01-47E7-9427-E7742B691F6B",
+                Name = UserRole.Guard.ToString(),
+                ConcurrencyStamp = "2",
+                NormalizedName = UserRole.Guard.ToString().ToUpper()
+            }, new IdentityRole
+            {
+                Id = "F46A30FC-9425-4275-A245-D389387D84A8",
+                Name = UserRole.Manager.ToString(),
+                ConcurrencyStamp = "3",
+                NormalizedName = UserRole.Manager.ToString().ToUpper()
+            }, new IdentityRole
+            {
+                Id = "89494DE6-9587-4EE5-B3F1-AC171C52911B",
+                Name = UserRole.Student.ToString(),
+                ConcurrencyStamp = "4",
+                NormalizedName = UserRole.Student.ToString().ToUpper()
+            });
+        }
+    }
+}
---
+++ backend/ReservationSystem.DataAccess/TypeConfigurations/UserTypeConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReservationSystem.DataAccess.Entities;
+
+namespace ReservationSystem.DataAccess.TypeConfigurations
+{
+    public class UserTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Surname)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.NormalizedEmail)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.PhoneNumber)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.UserName)
+                .HasMaxLength(200);
+
+            builder.Property(x => x.NormalizedUserName)
+                .HasMaxLength(200);
+
+            builder.HasMany(x => x.Reservations)
+                .WithOne(x => x.User);
+        }
+    }
+}
