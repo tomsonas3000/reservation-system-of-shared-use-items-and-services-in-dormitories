37aedb7209604052d35f80b8aa53cdd9ca0981d8
tomsonas3000 <tomasjorudas23@gmail.com>
2022-02-27 21:20:30 +0200

    Add header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
 import { ThemeProvider } from '@emotion/react';
-import { Container } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './components/auth/signIn/SignIn';
+import Header from './components/base/Header';
 
 const theme = createTheme();
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Container
-        maxWidth="xs"
-        sx={{
-          display: 'flex',
-        }}>
-        <SignIn />
-      </Container>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 };
---
+++ frontend/src/components/base/Header.tsx
@@ -0,0 +1,125 @@
+import {
+  AppBar,
+  Button,
+  MenuItem,
+  Toolbar,
+  Drawer,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const adminMenuItems = [
+  {
+    label: 'Dormitories',
+    href: '/dormitories',
+  },
+  {
+    label: 'Users',
+    href: '/users',
+  },
+  {
+    label: 'Services',
+    href: '/services',
+  },
+  {
+    label: 'Reservations',
+    href: '/reservations',
+  },
+];
+
+const Header = () => {
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      window.innerWidth < 600 ? setMobileView(true) : setMobileView(false);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <header>
+      <AppBar color="inherit">
+        {mobileView ? <MobileHeader /> : <DesktopHeader />}
+      </AppBar>
+    </header>
+  );
+};
+
+const DesktopHeader = (): JSX.Element => {
+  return (
+    <Toolbar color="inherit" sx={{ display: 'flex', justifyContent: 'center' }}>
+      <HeaderButtons />
+    </Toolbar>
+  );
+};
+
+const MobileHeader = (): JSX.Element => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <Toolbar sx={{ justifyContent: 'right' }} color="inherit">
+      <IconButton
+        color="inherit"
+        aria-label="menu"
+        aria-haspopup="true"
+        onClick={() => setDrawerOpen(!drawerOpen)}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        sx={{ padding: '2rem' }}
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(!drawerOpen)}>
+        <DrawerButtons />
+      </Drawer>
+    </Toolbar>
+  );
+};
+
+const HeaderButtons = (): JSX.Element => {
+  return (
+    <>
+      {adminMenuItems.map(({ label, href }) => {
+        return (
+          <Button
+            key={label}
+            to={href}
+            color="inherit"
+            component={Link}
+            sx={{ marginX: '2rem' }}>
+            {label}
+          </Button>
+        );
+      })}
+    </>
+  );
+};
+
+const DrawerButtons = (): JSX.Element => {
+  return (
+    <>
+      {adminMenuItems.map(({ label, href }) => {
+        return (
+          <Link
+            to={href}
+            color="black"
+            key={label}
+            style={{ textDecoration: 'none' }}>
+            <MenuItem>
+              <Typography color="black">{label}</Typography>
+            </MenuItem>
+          </Link>
+        );
+      })}
+    </>
+  );
+};
+
+export default Header;
